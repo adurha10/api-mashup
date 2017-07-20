1c8ef33c6f55b83b9281b94ad59d8c82200f7609
nwdavis <nathanieldavis89@gmail.com>
2017-07-20 18:43:48 -0400

    update

--- assets/javascript/fiddle.js
+++ assets/javascript/fiddle.js
@@ -1,10 +1,10 @@
 
-// var queryURL = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=172218&t.k=vkeqkMTFC7&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0"
+var queryURL = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=172218&t.k=vkeqkMTFC7&action=employers&q=webdeveloper&userip=192.168.43.42&useragent=Mozilla/%2F4.0"
 
-var queryURL = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?sort=1&sd=d&city=" + zipCode + "&text=" + searchParam
+// var queryURL = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?sort=1&sd=d&city=" + zipCode + "&text=" + searchParam
 
-var zipCode = 
-var searchParam = 
+var zipCode
+var searchParam
 
 $.ajax({
       url: queryURL,
