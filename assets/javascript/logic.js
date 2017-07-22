84847d3819c2a29b1fbb5750c1c3c64ca63c9423
adurha10 <alamadu82@gmail.com>
2017-07-22 13:20:34 -0400

    added infowidow for home location

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,4 @@
-document.ready
+
 var jobListings = [
     { 
     jobName: "Company X",
@@ -102,6 +102,56 @@ function initMap(){
         zoom:13
     });
 
+    var input = document.getElementById('zip-input');
+    console.log(input);
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    console.log(autocomplete);
+        autocomplete.bindTo('bounds', map);
+    var marker = new google.maps.Marker({
+        map: map,
+        icon: "assets/images/homeicon.png",
+        anchorPoint: new google.maps.Point(20, 20)
+    });
+
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindowContent.children['place-icon'].src = place.icon;
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-address'].textContent = address;
+        infowindow.open(map, marker);
+    });
+    
     infowindow = new google.maps.InfoWindow();
     
     google.maps.event.addDomListener(window, 'resize', initMap);
@@ -140,15 +190,15 @@ function createMarker(place) {
     }); 
 }
 
-function createHome(place) {
-    var placeLoc = place.geometry.location; 
-    var marker = new google.maps.Marker({ 
-        map: map,
-        icon: "assets/images/homeicon.png"
-        position: place.geometry.location 
-    }); 
-    google.maps.event.addListener(marker, 'click', function(){ 
-        infowindow.setContent(place.name); 
-        infowindow.open(map, this); 
-    }); 
-}
+// function createHome(place) {
+//     var placeLoc = place.geometry.location; 
+//     var marker = new google.maps.Marker({ 
+//         map: map,
+//         icon: "assets/images/homeicon.png"
+//         position: place.geometry.location 
+//     }); 
+//     google.maps.event.addListener(marker, 'click', function(){ 
+//         infowindow.setContent(place.name); 
+//         infowindow.open(map, this); 
+//     }); 
+// }
